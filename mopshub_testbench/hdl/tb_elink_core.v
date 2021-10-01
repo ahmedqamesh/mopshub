b6120cef879d21d0ad4f5b98d8f1701e40b9571a
ahmedqamesh <ahmedqamesh@github.com>
2021-10-01 19:10:05 +0200

    update

--- mopshub_testbench/hdl/tb_elink_core.v
+++ mopshub_testbench/hdl/tb_elink_core.v
@@ -1,11 +1,13 @@
 `resetall
-`timescale 1ns/10ps
+`timescale 1ns/100ps
 module tb_elink_core ;
   // Port Declarations
   reg          rst; 
-  reg          clk_160;  //clk_160 [clk_160 MB/s]    
+  reg          clk_enable;  //clk_enable [clk_enable MB/s]    
   wire         clk_80;  //clk_80 [clk_80 MB/s]
   wire         clk_40;    //clk_40   [clk_40 MB/s]
+  wire         clk_801;  //clk_80 [clk_80 MB/s]
+  wire         clk_401;    //clk_40   [clk_40 MB/s]
   //EPROC OUT ENC8b10b Signals
   wire  [7:0]  gen_edata_8bit;
   wire  [1:0]  delimeter;
@@ -22,15 +24,15 @@ module tb_elink_core ;
   assign data_emci_tra    = emci_decoder0.data_tra_76bit_reg ; 
   assign data_emci_rec    = emci_decoder0.data_rec_76bit_reg;
   assign end_write_elink  = emci_decoder0.end_write_emulator;
- 
-
+  //integer i;
+  
   //Generate 8b data 
 emci_decoder #(
-  .max_cnt(1))emci_decoder0(
+  .max_cnt(1),
+  .seialize_data_stream(1))emci_decoder0(
   .rst                    (rst),
-  .clk_160               (clk_160),
   .clk_80                 (clk_80),
-  .clk_40                 (clk_40),//I will use here bitCLK if I need to test elink_proc_out
+  .clk_40                 (clk_40),
   .loop_en                (1'b0),
   .start_write_emulator   (start_write_elink),
   .data_tra_out          (data_gen_tra), 
@@ -45,22 +47,35 @@ emci_decoder #(
   .data_10b_in            (10'b0),//{enc10bit_r[0],enc10bit_r[1],enc10bit_r[2],enc10bit_r[3],enc10bit_r[4],enc10bit_r[5],enc10bit_r[6],enc10bit_r[7],enc10bit_r[8],enc10bit_r[9]}), 
   .data_10b_en            (1'b0)
   );
-
-  // clocks    
+// clocks
   initial begin 
-    clk_160=0; 
-    forever #1 clk_160=~clk_160; //Equivalent to the rd_clk
+    clk_enable=0;
+    forever #(1) clk_enable=~clk_enable; //Equivalent to the rd_clk
   end
   
-  //Freq. bitCLK = Freq. clk_160 / 4 [=40 MHz]
+  clock_generator #(
+  .freq(80),//MHz
+  .phase(0),
+  .duty(50))clk_gen80(
+  .enable(clk_enable), 
+  .clk(clk_801));
+
+  clock_generator #(
+  .freq(40),//MHz
+  .phase(0),
+  .duty(50))clk_gen40(
+  .enable(clk_enable), 
+  .clk(clk_401));
+  
+  //Freq. bitCLK = Freq. clk_enable / 4 [=40 MHz]
   clock_divider #(4) div_4(
-  .clock_in(clk_160),
+  .clock_in(clk_enable),
   .clock_out(clk_40)
   );
   
   //Freq. bitCLK = Freq. clk_80 / 2 [=80 MHz]
   clock_divider #(2) div_2(
-  .clock_in(clk_160),
+  .clock_in(clk_enable),
   .clock_out(clk_80)//Equivalent to the 80
   );
   initial 
--- mopshub_testbench/hdl/tb_mopshub_top.v
+++ mopshub_testbench/hdl/tb_mopshub_top.v
@@ -1,236 +1,241 @@
 `resetall
 `timescale 1ns/10ps
 module tb_mopshub_top();
-reg             clk = 1'b0;
-wire             clk_40;
-wire             clk_80;
-reg             rst   = 1'b1;
-wire            rst_mops_dbg;
-reg             sel_bus = 1'b0;
-reg     [4:0]   can_tra_select_dbg =5'd5;
-wire            sign_on_sig;
-reg             start_data_gen= 1'b0;
-wire            start_init;
-wire            end_init;
-string          info_debug_sig;     
-//tbSM signals  
-wire    [75:0]  bus_dec_data;
-wire    [7:0]   bus_id;
-int             adc_ch;
+  reg             clk = 1'b0;
+  wire             clk_40;
+  wire             clk_80;
+  reg             rst   = 1'b1;
+  wire            rst_mops_dbg;
+  reg             sel_bus = 1'b1;
+  reg     [4:0]   can_tra_select_dbg =5'd1;
+  wire            sign_on_sig;
+  reg             start_data_gen= 1'b0;
+  wire            start_init;
+  wire            end_init;
+  string          info_debug_sig;     
+  //tbSM signals  
+  wire    [75:0]  bus_dec_data;
+  wire    [7:0]   bus_id;
+  int             adc_ch;
+  
+  //Automated trimming signals
+  reg             osc_auto_trim =1'b0; ////Active high. Enable /disable automated trimming. If disabled then take care of ftrim_pads_reg
+  wire            trim_sig_start;
+  wire            trim_sig_end;
+  wire            trim_sig_done;
+  
+  reg             osc_auto_trim_mopshub =1'b0;
+  wire            ready_osc;
+  wire            start_trim_osc;
+  wire            end_trim_bus;
+  wire            done_trim_osc;
+  
+  wire            power_bus_en;
+  wire    [4:0]   power_bus_cnt;
+  
+  wire            sign_in_start;
+  wire            sign_in_end;
+  
+  reg             test_osc_reg = 1'b0;
+  wire            osc_reg_start;
+  wire            osc_reg_end;
+  
+  reg             test_rx = 1'b0;
+  wire            test_rx_start;
+  wire            test_rx_end;
+  
+  reg             test_tx = 1'b0;
+  wire            test_tx_start;
+  wire            test_tx_end;
+  
+  // MOPSHUB signals
+  wire    [75:0]  data_rec_uplink;
+  wire    [75:0]  data_tra_emulator_out;
+  wire    [4:0]   can_rec_select;
+  wire    [75:0]  data_tra_uplink;
+  wire    [4:0]  can_tra_select;
+  wire    [75:0] data_rec_emulator_in;
+  wire            irq_elink_tra;
+  wire            irq_elink_rec;
+  reg      [75:0] requestreg  = 75'h0;
+  wire            reqmsg;
+  reg      [75:0] responsereg = 75'h0; 
+  wire            respmsg;
+  // Generator signals 
+  int failures = 0;   // Number of BAD reponses from the chip  
+  wire            rx0;
+  wire            rx1;
+  wire            rx2;
+  wire            rx3;
+  wire            rx4;
+  wire            rx5;
+  wire            rx6;
+  wire            rx7;
+  
+  wire            tx0;
+  wire            tx1;
+  wire            tx2;
+  wire            tx3;
+  wire            tx4;
+  wire            tx5;
+  wire            tx6;
+  wire            tx7;
+  wire [1:0] tx_mopshub_2bit; 
+  wire       tx_mopshub_1bit; 
+  wire [1:0] rx_mopshub_2bit; 
+  wire       rx_mopshub_1bit;
+  
+  //Internal assignments  
+  assign can_tra_select   = mopshub0.can_tra_select;
+  assign can_rec_select   = mopshub0.can_rec_select;
+  assign data_rec_uplink  = mopshub0.data_rec_uplink;
+  assign data_tra_uplink  = mopshub0.data_tra_uplink;
+  assign done_trim_osc    = mopshub0.done_trim_osc;
+  assign start_init       = mopshub0.start_init;
+  assign end_init         = mopshub0.end_init;
+  assign rst_mops_dbg     = mopshub0.rst_mops_dbg;
+  assign sign_on_sig      = mopshub0.sign_on_sig;
+  assign end_trim_bus     = mopshub0.end_trim_bus;
+  assign start_trim_osc   = mopshub0.start_trim_ack;
+  
+  
+  assign data_tra_emulator_out  = data_generator0.data_tra_76bit_reg;
+  assign data_rec_emulator_in   = data_generator0.data_rec_76bit_reg;
+  assign ready_osc              = data_generator0.ready_osc;
+  
+  assign irq_elink_tra = mopshub0.irq_elink_tra;
+  assign irq_elink_rec = mopshub0.irq_elink_rec;
+  
+  mopshub_top#(
+  .n_buses (5'b111),
+  .seialize_data_stream(1))mopshub0(
+  .clk(clk_40),
+  //.clk_40(clk_40),
+  .clk_80(clk_80),
+  .rst(rst),  
+  .osc_auto_trim_mopshub(osc_auto_trim_mopshub), 
+  .power_bus_en(power_bus_en),
+  .power_bus_cnt(power_bus_cnt),                     
+  .end_cnt_dbg(1'b0),
+  .can_tra_select_dbg(can_tra_select_dbg),              
+  .tx_elink2bit(tx_mopshub_2bit),
+  .tx_elink1bit(tx_mopshub_1bit),
+  .rx_elink1bit(rx_mopshub_1bit),
+  .rx_elink2bit(rx_mopshub_2bit),        
+  .rx0(rx0),        
+  .rx1(rx1),        
+  .rx2(rx2),        
+  .rx3(rx3),        
+  .rx4(rx4),        
+  .rx5(rx5),        
+  .rx6(rx6),        
+  .rx7(rx7),
+  .tx0(tx0),              
+  .tx1(tx1),
+  .tx2(tx2),
+  .tx3(tx3),
+  .tx4(tx4),
+  .tx5(tx5),
+  .tx6(tx6),
+  .tx7(tx7));
+  
+  data_generator#(
+  .n_buses (5'b111),
+  .seialize_data_stream(1))data_generator0(
+  .clk(clk_40),
+  //.clk_80(clk_80),
+  .rst(rst),
+  .ext_rst_mops(rst_mops_dbg),
+  .ext_trim_mops(osc_auto_trim_mopshub),
+  .loop_en(1'b0),
+  //Start SM
+  .start_data_gen(start_data_gen),
+  //OScillation Triming Signals
+  .osc_auto_trim(osc_auto_trim),
+  .trim_sig_start(trim_sig_start),
+  .trim_sig_end (trim_sig_end),
+  .trim_sig_done(trim_sig_done),
+  .sign_in_start(sign_in_start), 
+  .sign_in_end(sign_in_end),
+  .osc_reg_end(osc_reg_end),
+  .osc_reg_start(osc_reg_start),
+  //Read ADC channels from MOPS and send it to MOPSHUB rx
+  .test_rx(test_rx),
+  .test_tx(test_tx),
+  .test_tx_end(test_tx_end),
+  .test_rx_start(test_rx_start),
+  .test_rx_end(test_rx_end),
+  .test_tx_start(test_tx_start),
+  .respmsg(respmsg),
+  .reqmsg(reqmsg),
+  .adc_ch(adc_ch),  
+  // Acknowledgement bit from the MOPSHUB
+  //Decoder Signals [Listen always to the bus ]
+  .bus_dec_data(bus_dec_data),
+  //read data from Elink and send it to the bus
+  .sel_bus(sel_bus),
+  .bus_cnt(can_tra_select_dbg),// test Bus 0
+  .test_mopshub_core(1'b0),
+  .irq_can_ack(1'b0),
+  .bus_id(bus_id),
+  .buffer_en(),
+  .test_elink_data_done(),
+  .start_write_emulator(),
+  .start_read_elink(),
+  .end_read_elink(),
+  //Elin.kSignals
+  .tx_elink1bit(rx_mopshub_1bit),
+  .tx_elink2bit(rx_mopshub_2bit),
+  .rx_elink1bit(tx_mopshub_1bit),
+  .rx_elink2bit(tx_mopshub_2bit),
+  //RX-TX signals
+  .rx0(rx0),        
+  .rx1(rx1),        
+  .rx2(rx2),        
+  .rx3(rx3),        
+  .rx4(rx4),        
+  .rx5(rx5),        
+  .rx6(rx6),        
+  .rx7(rx7),
+  .tx0(tx0),              
+  .tx1(tx1),
+  .tx2(tx2),
+  .tx3(tx3),
+  .tx4(tx4),
+  .tx5(tx5),
+  .tx6(tx6),
+  .tx7(tx7));
+  //////////****// Clock generation////////////////
+  always #50 clk = ~clk;   
+  //////////////////////////////////////////////// 
+  
+  clock_divider #(28'd4)
+  clock_divider4( 
+  .clock_in  (clk), 
+  .clock_out (clk_40)
+  ); 
+  
+  
+  clock_divider #(28'd2)
+  clock_divider2( 
+  .clock_in  (clk), 
+  .clock_out (clk_80)
+  ); 
 
-//Automated trimming signals
-reg             osc_auto_trim =1'b0; ////Active high. Enable /disable automated trimming. If disabled then take care of ftrim_pads_reg
-wire            trim_sig_start;
-wire            trim_sig_end;
-wire            trim_sig_done;
-
-reg             osc_auto_trim_mopshub =1'b0;
-wire            ready_osc;
-wire            start_trim_osc;
-wire            end_trim_bus;
-wire            done_trim_osc;
-
-wire            power_bus_en;
-wire    [4:0]   power_bus_cnt;
-
-wire            sign_in_start;
-wire            sign_in_end;
-
-reg             test_osc_reg = 1'b0;
-wire            osc_reg_start;
-wire            osc_reg_end;
-
-reg             test_rx = 1'b0;
-wire            test_rx_start;
-wire            test_rx_end;
-
-reg             test_tx = 1'b0;
-wire            test_tx_start;
-wire            test_tx_end;
-
-// MOPSHUB signals
-wire    [75:0]  data_rec_uplink;
-wire    [75:0]  data_tra_emulator_out;
-wire    [4:0]   can_rec_select;
-wire    [75:0]  data_tra_uplink;
-wire    [4:0]  can_tra_select;
-wire    [75:0] data_rec_emulator_in;
-wire            irq_elink_tra;
-wire            irq_elink_rec;
-reg      [75:0] requestreg  = 75'h0;
-wire            reqmsg;
-reg      [75:0] responsereg = 75'h0; 
-wire            respmsg;
-// Generator signals 
-int failures = 0;   // Number of BAD reponses from the chip  
-wire            rx0;
-wire            rx1;
-wire            rx2;
-wire            rx3;
-wire            rx4;
-wire            rx5;
-wire            rx6;
-wire            rx7;
-
-wire            tx0;
-wire            tx1;
-wire            tx2;
-wire            tx3;
-wire            tx4;
-wire            tx5;
-wire            tx6;
-wire            tx7;
-//wire [1:0] tx_mopshub_2bit; 
-wire       tx_mopshub_1bit; 
-//wire [1:0] rx_mopshub_2bit; 
-wire       rx_mopshub_1bit;
-
-//Internal assignments  
-assign can_tra_select   = mopshub0.can_tra_select;
-assign can_rec_select   = mopshub0.can_rec_select;
-assign data_rec_uplink  = mopshub0.data_rec_uplink;
-assign data_tra_uplink  = mopshub0.data_tra_uplink;
-assign done_trim_osc    = mopshub0.done_trim_osc;
-assign start_init       = mopshub0.start_init;
-assign end_init         = mopshub0.end_init;
-assign rst_mops_dbg     = mopshub0.rst_mops_dbg;
-assign sign_on_sig      = mopshub0.sign_on_sig;
-assign end_trim_bus     = mopshub0.end_trim_bus;
-assign start_trim_osc   = mopshub0.start_trim_ack;
-
-
-assign data_tra_emulator_out  = data_generator0.data_tra_76bit_reg;
-assign data_rec_emulator_in   = data_generator0.data_rec_76bit_reg;
-assign ready_osc              = data_generator0.ready_osc;
-
-assign irq_elink_tra = mopshub0.irq_elink_tra;
-assign irq_elink_rec = mopshub0.irq_elink_rec;
-
-mopshub_top#(
-.n_buses (5'b111))mopshub0(
-.clk(clk_40),
-.clk_40(clk_40),
-.clk_80(clk_80),
-.rst(rst),  
-.osc_auto_trim_mopshub(osc_auto_trim_mopshub), 
-.power_bus_en(power_bus_en),
-.power_bus_cnt(power_bus_cnt),                     
-.end_cnt_dbg(1'b0),
-.can_tra_select_dbg(can_tra_select_dbg),              
-//.tx_elink2bit(tx_mopshub_2bit),
-.tx_elink1bit(tx_mopshub_1bit),
-.rx_elink1bit(rx_mopshub_1bit),
-//.rx_elink2bit(rx_mopshub_2bit),        
-.rx0(rx0),        
-.rx1(rx1),        
-.rx2(rx2),        
-.rx3(rx3),        
-.rx4(rx4),        
-.rx5(rx5),        
-.rx6(rx6),        
-.rx7(rx7),
-.tx0(tx0),              
-.tx1(tx1),
-.tx2(tx2),
-.tx3(tx3),
-.tx4(tx4),
-.tx5(tx5),
-.tx6(tx6),
-.tx7(tx7));
-
-data_generator#(
-.n_buses (5'b111))data_generator0(
-.clk(clk),
-.rst(rst),
-.ext_rst_mops(rst_mops_dbg),
-.ext_trim_mops(osc_auto_trim_mopshub),
-.loop_en(1'b0),
-//Start SM
-.start_data_gen(start_data_gen),
-//OScillation Triming Signals
-.osc_auto_trim(osc_auto_trim),
-.trim_sig_start(trim_sig_start),
-.trim_sig_end (trim_sig_end),
-.trim_sig_done(trim_sig_done),
-.sign_in_start(sign_in_start), 
-.sign_in_end(sign_in_end),
-.osc_reg_end(osc_reg_end),
-.osc_reg_start(osc_reg_start),
-//Read ADC channels from MOPS and send it to MOPSHUB rx
-.test_rx(test_rx),
-.test_tx(test_tx),
-.test_tx_end(test_tx_end),
-.test_rx_start(test_rx_start),
-.test_rx_end(test_rx_end),
-.test_tx_start(test_tx_start),
-.respmsg(respmsg),
-.reqmsg(reqmsg),
-.adc_ch(adc_ch),  
-// Acknowledgement bit from the MOPSHUB
-//Decoder Signals [Listen always to the bus ]
-.bus_dec_data(bus_dec_data),
-//read data from Elink and send it to the bus
-.sel_bus(sel_bus),
-.bus_cnt(can_tra_select_dbg),// test Bus 0
-.test_mopshub_core(1'b0),
-.irq_can_ack(1'b0),
-.bus_id(bus_id),
-.buffer_en(),
-.test_elink_data_done(),
-.start_write_emulator(),
-.start_read_elink(),
-.end_read_elink(),
-//Elin.kSignals
-.tx_elink1bit(rx_mopshub_1bit),
-//.tx_elink2bit(rx_mopshub_2bit),
-.rx_elink1bit(tx_mopshub_1bit),
-//.rx_elink2bit(tx_mopshub_2bit),
-//RX-TX signals
-.rx0(rx0),        
-.rx1(rx1),        
-.rx2(rx2),        
-.rx3(rx3),        
-.rx4(rx4),        
-.rx5(rx5),        
-.rx6(rx6),        
-.rx7(rx7),
-.tx0(tx0),              
-.tx1(tx1),
-.tx2(tx2),
-.tx3(tx3),
-.tx4(tx4),
-.tx5(tx5),
-.tx6(tx6),
-.tx7(tx7));
-//////////****// Clock generation////////////////
-always #50 clk = ~clk;   
-//////////////////////////////////////////////// 
-clock_divider #(28'd2)
-clock_divider2( 
-   .clock_in  (clk), 
-   .clock_out (clk_80)
-); 
-
-clock_divider #(28'd4)
-clock_divider4( 
-   .clock_in  (clk), 
-   .clock_out (clk_40)
-); 
- 
   /////******* Reset Generator task--low active ****/////
   initial 
   begin
     rst = 1'b0;
     #200
     rst = 1'b1;
- end
-always@(posedge sign_on_sig)
-     begin
-     #1500
-     start_data_gen = 1'b1;
-     #100
-     start_data_gen = 1'b0;
-    end  
+  end
+  always@(posedge sign_on_sig)
+  begin
+    #1500
+    start_data_gen = 1'b1;
+    #100
+    start_data_gen = 1'b0;
+  end  
   /////*******Start Full SM for Data Generation ****/////
   always@(posedge clk)
   begin  
@@ -244,7 +249,7 @@ always@(posedge sign_on_sig)
     begin
       test_rx =1'b1;
       test_osc_reg = 1'b0;
-      // test_tx =1'b1;
+      test_tx =1'b0;
       start_data_gen =1'b0;
     end
     if(test_rx_end ==1)//Done Rx test
@@ -327,7 +332,7 @@ always@(posedge sign_on_sig)
       $strobe("*****************************************************************************");
       info_debug_sig = {""};
     end 
-        /////*********************************  TX Test    *********************************/////     
+    /////*********************************  TX Test    *********************************/////     
     if (test_tx_start)
     begin 
       info_debug_sig = $sformatf("<:TX signals  [BUS ID %d ]  :>",bus_id);
@@ -354,13 +359,13 @@ always@(posedge sign_on_sig)
     else if(respmsg && osc_auto_trim  && !test_rx && !test_tx && !test_osc_reg)
     begin
       responsereg <= data_rec_uplink;
-       $strobeh("\t Oscillator trimming [BUS ID %d]: \t request %h \t response %h \t",bus_id,requestreg,responsereg);
+      $strobeh("\t Oscillator trimming [BUS ID %d]: \t request %h \t response %h \t",bus_id,requestreg,responsereg);
     end  
     else if(reqmsg && osc_auto_trim  && !test_rx && !test_tx && !test_osc_reg)
     begin
       requestreg <= {76'h555aaaaaaaaaaaaaaaa};  
     end 
-     
+    
     //test RX Response part 
     else if(reqmsg && (test_rx || test_osc_reg))
     begin
@@ -398,7 +403,7 @@ always@(posedge sign_on_sig)
   begin 
     if (respmsg)
     begin
-     #500
+      #500
       casez(requestreg)
         75'h0: begin    //////// Reset requestreg////
           if(responsereg inside {{43'h701?5000000,bus_id,24'h0}})
@@ -705,15 +710,15 @@ always@(posedge sign_on_sig)
         
         //// Below is the check for write operation 
         ///////**********************************///
-
+        
         
         default:
         begin 
           if(responsereg == requestreg)
           begin
-          $strobe("Status GOOD [BUS ID %d]- test",bus_id);
-          $strobe("responsereg = responsereg [BUS ID %d]: Msg = %h",bus_id, responsereg);
-        end
+            $strobe("Status GOOD [BUS ID %d]- test",bus_id);
+            $strobe("responsereg = responsereg [BUS ID %d]: Msg = %h",bus_id, responsereg);
+          end
           else
           begin
             $display("Current simulation time is: ", $realtime);
