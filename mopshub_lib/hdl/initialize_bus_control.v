7d67bc5853cc0ebacffaae5f1cceeb6590d418e0
ahmedqamesh <ahmedqamesh@github.com>
2022-02-15 13:00:25 +0100

    update power module

--- mopshub_lib/hdl/initialize_bus_control.v
+++ mopshub_lib/hdl/initialize_bus_control.v
@@ -15,8 +15,6 @@ module initialize_bus_control(
    output  wire    [15:0]  data_init  // Ouput data to be written to the registers
 );
 
-//tmrg default triplicate
-//tmrg tmr_error false
 // Internal Declarations
 reg [15:0] data_init_reg;
 assign data_init = data_init_reg;
@@ -24,8 +22,8 @@ assign data_init = data_init_reg;
 always @ (*)
 begin
    case(addr)
-       5'b01111 : data_init_reg = 16'h00FF;          //  prescalar register value 
-       5'b01110 : data_init_reg = 16'h00A3;          // general register.first 7bits=0 then sjw,tseg1,tseg2 each 3bits [0000000 010 100 011]
+       5'b01111 : data_init_reg = 16'h00FF;          //prescalar register value 
+       5'b01110 : data_init_reg = 16'h00A3;          //general register.first 7bits=0 then sjw,tseg1,tseg2 each 3bits [0000000 010 100 011]
        5'b00101 : data_init_reg = 16'h0000;          //ID 28-13 
        5'b00100 : data_init_reg = 16'h0000;          //ID 12-0 
        5'b10010 : data_init_reg = 16'h8070;          // Enable communication by setting the MSB of interrupt register and enable irqs
