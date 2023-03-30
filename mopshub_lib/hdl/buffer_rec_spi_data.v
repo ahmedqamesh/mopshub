f6f4035bb3d8c89f7eb40beece100982809cb90a
aqamesh <ahmed.qamesh@cern.ch>
2023-03-30 14:49:03 +0200

    More development in the SPI side

--- mopshub_lib/hdl/buffer_rec_spi_data.v
+++ mopshub_lib/hdl/buffer_rec_spi_data.v
@@ -19,15 +19,21 @@ reg [7:0] id;      // CAN ID is 11-bits.  Here 3 Hex values are used to reprent 
 reg [7:0] b1 ;       // Data bytes 1...8
 reg [7:0] b2;
 reg [7:0] b3;
+reg [7:0] b4;
+reg [7:0] b5;
+reg [7:0] b6;
+reg [7:0] b7;
 reg end_read_miso_reg;
-initial 
-begin
-  id = 8'h0;
-  b1 = 8'h0;
-  b2 = 8'h0;
-  b3 = 8'h0;
-  end_read_miso_reg = 1'b0;
-end
+
+initial  id = 8'h0;
+initial  b1 = 8'h0;
+initial  b2 = 8'h0;
+initial  b3 = 8'h0;
+initial  b4 = 8'h0;
+initial  b5 = 8'h0;
+initial  b6 = 8'h0;
+initial  b7 = 8'h0;
+initial  end_read_miso_reg = 1'b0;
 always@(posedge clk)
 begin 
   if(!rst)
@@ -36,14 +42,18 @@ begin
    b1 <= 8'h00;
    b2 <= 8'h00;
    b3 <= 8'h00;
+   b4 <= 8'h00;
+   b5 <= 8'h00;
+   b6 <= 8'h00;
+   b7 <= 8'h00;
    end_read_miso_reg =1'b0;
    end
   if(buffer_en)
     begin
-      id  <= spi_id_in;        // 5-bit selection line is the address of corresponding registers in Cankari. ID register data
-      b1  <= spi_select ;            // first  data byte          
-      b2  <= spi_reg;             // second data byte
-      b3  <= data_rec_in;             // third data byte
+      id  <= spi_id_in;       // The ID of the Monitoring SPI
+      b1  <= spi_select ;     // Define the selected bus 0-16    
+      b2  <= spi_reg;         // Define the Regester 
+      b3  <= data_rec_in;     // Data Received 
       end_read_miso_reg =1'b1;
       end
   else
