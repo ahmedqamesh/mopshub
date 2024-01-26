26101559cecec66a5418f1d488b0993f294543f1
aqamesh <ahmed.qamesh@cern.ch>
2024-01-26 12:22:08 +0100

    Hello update

--- mopshub_update_git.sh
+++ mopshub_update_git.sh
@@ -1,3 +1,6 @@
+
+$1="Commit"$now
+
 echo "Update Submodules...."
 # Based on https://git-scm.com/book/en/v2/Git-Tools-Submodules
 git status
@@ -7,5 +10,29 @@ cd ipbus-firmware
 git fetch
 git merge origin/master
 cd ..
+echo "Pulling in Upstream Changes from the Submodule mopshub-canakari"
+cd mopshub-canakari
+git fetch
+git merge origin/master
+cd ..
+echo "Pulling in Upstream Changes from the Submodule mopshub-reconfig"
+cd mopshub-reconfig
+git fetch
+git merge origin/master
+cd ..
+echo "Pulling in Upstream Changes from the Submodule mopshub-seu-test"
+cd mopshub-seu-test
+git fetch
+git merge origin/master
+cd ..
+echo "Pulling in Upstream Changes from the Submodule mopshub-triplicated"
+cd mopshub-triplicated
+git fetch
+git merge origin/master
+cd ..
 
-
+echo "Push local Changes to the Remote"
+git add -A
+git commit -m $COMMIT
+git push gitlab
+git push github
