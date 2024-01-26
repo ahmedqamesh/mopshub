b60c04da2c3ee9e10350b30044ff0e57e9a39c40
aqamesh <ahmed.qamesh@cern.ch>
2024-01-26 12:14:54 +0100

    update ipbus

--- mopshub_update_git.sh
+++ mopshub_update_git.sh
@@ -1,3 +1,11 @@
 echo "Update Submodules...."
 # Based on https://git-scm.com/book/en/v2/Git-Tools-Submodules
 git status
+git submodule update --init --recursive
+echo "Pulling in Upstream Changes from the Submodule ipbus-firmware"
+cd ipbus-firmware
+git fetch
+git merge origin/master
+cd ..
+
+
