ea665ff68c8fd0e35be938f0dc08c2012a1d6405
aqamesh <ahmed.qamesh@cern.ch>
2024-04-02 10:57:54 +0200

    readme

---
+++ Vivado/mopshub_v1/mopshub_board_v1/mopshub_board_v1.runs/mopshub_board_v1_selectio_wiz_2_0_synth_1/mopshub_board_v1_selectio_wiz_2_0_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.2 (lin64) Build 3064766 Wed Nov 18 09:12:47 MST 2020
+// Date        : Tue Mar 26 14:32:25 2024
+// Host        : chipdev2.physik.uni-wuppertal.de running 64-bit unknown
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ mopshub_board_v1_selectio_wiz_2_0_stub.v
+// Design      : mopshub_board_v1_selectio_wiz_2_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a200tfbg484-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(data_out_from_device, data_out_to_pins_p, 
+  data_out_to_pins_n, clk_in, clk_div_in, io_reset)
+/* synthesis syn_black_box black_box_pad_pin="data_out_from_device[1:0],data_out_to_pins_p[0:0],data_out_to_pins_n[0:0],clk_in,clk_div_in,io_reset" */;
+  input [1:0]data_out_from_device;
+  output [0:0]data_out_to_pins_p;
+  output [0:0]data_out_to_pins_n;
+  input clk_in;
+  input clk_div_in;
+  input io_reset;
+endmodule
